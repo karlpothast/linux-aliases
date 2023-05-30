2368e3f8a4becb16802054710d91a333cb76923d
Karl Pothast <karlpothast@gmail.com>
2023-05-30 03:52:40 -0500

    for createhtml template command

---
+++ files/html5+css+js/index.js
@@ -0,0 +1,9 @@
+const initialize = async () => {
+	try {
+    //console.log("index.js initialized")
+  } 
+  catch(error) { 
+    console.log(error);
+  }
+}
+window.addEventListener('DOMContentLoaded', initialize)
